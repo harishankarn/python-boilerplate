3dd46656995f2e883cb6df8fe8f5d5a1e68aac40
BtMn <btmn@BtMns-MacBook-Pro.local>
2023-07-19 12:19:50 +0530

    fixes

--- src/testing/tesing.py
+++
@@ -1,6 +0,0 @@
-
-  iContent-Length': '266', 'Access-Control-Allow-Origin': '*', 
-  'Access-Control-Allow-Credentials': 'true', 'Via': '1.1 vegur'}
-
-
---------------------------------------------------------------------------------
